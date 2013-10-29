523358a71ce9e0fd8d02800eda823ff98ded4e0a
Heitor Maffra <heitormaffra@gmail.com>
2013-10-29 16:31:58 -0200

    Alterações na classe pra não trazer informações do banco.

--- src/main/java/backing/bean/atividade/AtividadeBean.java
+++ src/main/java/backing/bean/atividade/AtividadeBean.java
@@ -27,8 +27,8 @@ public class AtividadeBean {
         ativdsSelecteds = new ArrayList<Atividade>();
         atividades = new ArrayList<Atividade>();
         atividades.add(new Atividade(1, "Coding", 50d, new Desenvolvedor(1, "Heitor", 1.5f)));
-        atividades.add(new Atividade(2, "QA", 50d, new Desenvolvedor(1, "Lucas", 1.5f)));
-        atividades.add(new Atividade(3, "Homolog", 50d, new Desenvolvedor(1, "Thiago", 1.5f)));
+        atividades.add(new Atividade(2, "QA", 10d, new Desenvolvedor(1, "Lucas", 1.5f)));
+        atividades.add(new Atividade(3, "Homolog", 30d, new Desenvolvedor(1, "Thiago", 1.5f)));
         atividades.get(1).setAtividadePrecedente(atividades.get(0));
         atividades.get(2).setAtividadePrecedente(atividades.get(1));
         
